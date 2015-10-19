ce32655322349aec50854e7a78137684f36f90a5
Fernando Faria <fvf@fernandovfaria.com>
2015-10-19 12:21:50 -0400

    Header File

---
+++ refstr.c
@@ -0,0 +1,28 @@
+import "refstr.h"
+
+int main(int argc, char** argv) {
+
+
+
+
+}
+
+
+char* genSpatial(int pageLimit, int refSize) {
+
+}
+
+char* genTemporal(int pageLimit, int refSize) {
+
+}
+
+char* genSpatialTemporal(int pageLimit, int refSize) {
+
+}
+
+char* genRandom(int pageLimit, int refSize) {
+
+}
+
+
+
---
+++ refstr.h
@@ -0,0 +1,6 @@
+
+char* genSpatial(int pageLimit, int refSize);
+char* genTemporal(int pageLimit, int refSize);
+char* genSpatialTemporal(int pageLimit, int refSize);
+char* genRandom(int pageLimit, int refSize);
+
